9403a90fb23a8668bd526ca6953e645537660dce
knight-ll <Knight>
2017-10-14 22:07:16 +0800

    第一次提交

---
+++ src/main/java/com/learn/housePrice/HousePriceApplication.java
@@ -0,0 +1,12 @@
+package com.learn.housePrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousePriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousePriceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/learn/housePrice/controller/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.learn.housePrice.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/springboot")
+public class HelloWorldController {
+
+
+    
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+    
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String sayWorld(@PathVariable("name") String name) {
+        return "Hello " + name;
+    }
+    
+}
